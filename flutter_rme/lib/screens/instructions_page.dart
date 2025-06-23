aed766943f88b2dde76272a528fbfc76351c32c2
danielyang21 <danielyanxiaoyang21@gmail.com>
2025-06-23 13:40:31 -0400

    Update instructions_page.dart

--- flutter_rme/lib/screens/instructions_page.dart
+++ flutter_rme/lib/screens/instructions_page.dart
@@ -32,6 +32,45 @@ class InstructionsPage extends StatelessWidget {
                   indent: 15, // Left margin
                   endIndent: 15, // Right margin
                 ),
+                
+                const Text(
+                  'Properties Page',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'This page display the properties of the first selected analyte. Here you can view a picture of the molecule, IUPAC name, molecular formula, molecular weight, polarity and other available data. The PubChem link will take you to the PubChem page for the molecule on an external browser, where you can find more information.',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 16, height: 1.5),
+                ),
+                const SizedBox(height: 10),
+                const Divider(
+                  height: 40,
+                  thickness: 1,
+                  color: Colors.grey,
+                  indent: 15, // Left margin
+                  endIndent: 15, // Right margin
+                ),
+
+                const Text(
+                  'Spectrum Page',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'This page displays the spectrum of the first selected analyte (if it\'s available). The spectrum is displayed as a graph, with the x-axis representing the retention time and the y-axis representing the intensity. Below the graph there is a table with the peaks detected in the spectrum, which can be sorted by retention time or intensity.',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 16, height: 1.5),
+                ),
+                const SizedBox(height: 10),
+                const Divider(
+                  height: 40,
+                  thickness: 1,
+                  color: Colors.grey,
+                  indent: 15, // Left margin
+                  endIndent: 15, // Right margin
+                ),
+
                 const Text(
                   'Polarity-MW Plot Page',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
