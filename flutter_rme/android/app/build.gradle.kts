e5e26221cda8b25ebb35c54cf1c082eba46e8af2
danielyang21 <danielyanxiaoyang21@gmail.com>
2025-08-14 11:45:17 -0400

    Update build.gradle.kts

--- flutter_rme/android/app/build.gradle.kts
+++ flutter_rme/android/app/build.gradle.kts
@@ -30,8 +30,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_rme"
+        applicationId = "com.metrology.flutter_rme"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
